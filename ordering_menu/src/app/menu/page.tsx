374d6dd72fd4af3b8861a22b4bd2b5a0a35c4af2
Firdaus <fdaus8080@gmail.com>
2025-06-13 16:47:21 +0800

    ran npm run build

--- ordering_menu/src/app/menu/page.tsx
+++ ordering_menu/src/app/menu/page.tsx
@@ -15,7 +15,7 @@ export default function Page() {
     />
     </div>
     <p className="mt-4 max-w-md text-center text-gray-700">
-        The picture above is "Nasi Goreng", which means fried rice. It is a traditional food of many countries, in the picture is one from a South East Asian country named "Brunei Darussalam"
+        The picture above is &quot;Nasi Goreng&quot;, which means fried rice. It is a traditional food of many countries, in the picture is one from a South East Asian country named &quot;Brunei Darussalam&quot;
     </p>
     <p className="mt-2 text-lg font-semibold text-green-600">BND 14.50</p>
     <button className="mt-4 bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">
@@ -34,7 +34,7 @@ export default function Page() {
     />
     </div>
     <p className="mt-4 max-w-md text-center text-gray-700">
-        The picture above is "Sup Ayam", which means chicken soup. It is a traditional food of many countries, in the picture is one from a South East Asian country named "Brunei Darussalam"
+        The picture above is &quot;Sup Ayam&quot;, which means chicken soup. It is a traditional food of many countries, in the picture is one from a South East Asian country named &quot;Brunei Darussalam&quot;
     </p>
     <p className="mt-2 text-lg font-semibold text-green-600">BND 14.50</p>
     <button className="mt-4 bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">
@@ -53,7 +53,7 @@ export default function Page() {
     />
     </div>
     <p className="mt-4 max-w-md text-center text-gray-700">
-        The picture above is "Ikan Goreng", which means fried fish. It is a fried food of many countries, in the picture is one from a South East Asian country named "Brunei Darussalam"
+        The picture above is &quot;Ikan Goreng&quot;, which means fried fish. It is a fried food of many countries, in the picture is one from a South East Asian country named &quot;Brunei Darussalam&quot;
     </p>
     <p className="mt-2 text-lg font-semibold text-green-600">BND 10.50</p>
     <button className="mt-4 bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">
