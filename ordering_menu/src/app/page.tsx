a00cfa696e077d17134a3f73af04bbee26063ebc
Firdaus <fdaus8080@gmail.com>
2025-06-14 12:02:39 +0800

    first commit to staging branch

--- ordering_menu/src/app/page.tsx
+++ ordering_menu/src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
           <li className="mb-2 tracking-[-.01em]">
             Menu ordering prototype{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] italic">
-            navigate to /menu
+            navigate to /menu by clicking the right button, not left
             </code>
             .
           </li>
